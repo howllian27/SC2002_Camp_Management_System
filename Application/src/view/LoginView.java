7697c7b2b90ad402315693bd629da4728910d8c9
chenglin2003 <133096344+chenglin2003@users.noreply.github.com>
2023-10-28 11:45:36 +0800

    commit login view

--- Application/src/view/LoginView.java
+++ Application/src/view/LoginView.java
@@ -5,16 +5,12 @@ import java.util.Scanner;
 public class LoginView {
     public void displayLogin() {
         // Code to display the login page
-        
+        System.out.println("  ____  ____  _____\n"
+                         + " / ___||  _ \\| ____|\n"
+                         + "| |    | | | |  _|\n"
+                         + "| |___ | |_| | |___\n"
+                         + " \\____||____/|_____|\n");
         System.out.println("Welcome to CAMs");
-        System.out.println("  ____       _           ______ _    _ ");
-        System.out.println(" |  _ \\     | |          |  ____| |  | |");
-        System.out.println(" | |_) | ___| |_   _     | |__  | |__| |");
-        System.out.println(" |  _ < / _ \\ | | | |    |  __| |  __  |");
-        System.out.println(" | |_) |  __/ | |_| |    | |____| |  | |");
-        System.out.println(" |____/ \\___|_|\\__, |    |______|_|  |_|");
-        System.out.println("               __/ |                  ");
-        System.out.println("              |___/                   ");
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter your user ID: ");
         String userId = scanner.nextLine();
