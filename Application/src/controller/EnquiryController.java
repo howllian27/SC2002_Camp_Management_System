ece9039864dc3a2631db96b62d5a560a65ee46b3
Howllian27 <67510209+howllian27@users.noreply.github.com>
2023-11-02 17:58:22 +0800

    update controllers

---
+++ Application/src/controller/EnquiryController.java
@@ -0,0 +1,61 @@
+package controller;
+
+import database.EnquiryDB;
+import model.Enquiry;
+import view.EnquiriesView;
+
+/**
+ * The EnquiryController class is responsible for handling enquiries from users.
+ * @author Chan Hin Wai Howell
+ * @version 1.0
+ * @since 2023-11-02
+ */
+public class EnquiryController implements BaseController {
+
+    private EnquiryDB enquiryDB;
+    private EnquiriesView enquiriesView;
+
+    public EnquiryController() {
+        setMasterVariables();
+    }
+
+    @Override
+    public void setMasterVariables() {
+        this.enquiryDB = new EnquiryDB();
+        this.enquiriesView = new EnquiriesView();
+    }
+
+    /**
+     * Allows a user to submit an enquiry.
+     *
+     * @param userID The ID of the user.
+     * @param campID The ID of the camp.
+     * @param enquiryText The text of the enquiry.
+     */
+    public void submitEnquiry(String userID, String campID, String enquiryText) {
+        Enquiry enquiry = new Enquiry(campID, userID, enquiryText);
+        enquiryDB.addEnquiry(enquiry);
+        enquiriesView.displayEnquirySubmissionSuccess();
+    }
+
+    /**
+     * Allows staff to view enquiries related to a camp.
+     *
+     * @param campID The ID of the camp.
+     */
+    public void viewEnquiries(String campID) {
+        var enquiries = enquiryDB.getEnquiries(campID);
+        enquiriesView.displayEnquiries(enquiries);
+    }
+
+    /**
+     * Allows staff or committee members to reply to an enquiry.
+     *
+     * @param enquiryID The ID of the enquiry.
+     * @param replyText The reply text.
+     */
+    public void replyToEnquiry(String enquiryID, String replyText) {
+        enquiryDB.updateEnquiryReply(enquiryID, replyText);
+        enquiriesView.displayReplySuccess();
+    }
+}
