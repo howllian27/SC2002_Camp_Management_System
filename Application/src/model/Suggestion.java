63c5207b9d50152e3239f79c57fef53186e203de
Ruin9999 <b14ktoss@gmail.com>
2023-11-13 19:20:13 +0800

    Added suggestion model

---
+++ Application/src/model/Suggestion.java
@@ -0,0 +1,35 @@
+package model;
+
+public class Suggestion {
+    private final String campId;
+    private final String studentId;
+    private CampInformation campInformation;
+    private String suggestionText;
+    private boolean isApproved;
+
+    public Suggestion(String studentId, String campId, String suggestionText, boolean isApproved) {
+        this.studentId = studentId;
+        this.campId = campId;
+        this.suggestionText = suggestionText;
+        this.isApproved = isApproved;
+    }
+
+    public Suggestion(String studentId, String campId, CampInformation campInformation, boolean isApproved) {
+        this.studentId = studentId;
+        this.campId = campId;
+        this.campInformation = campInformation;
+        this.isApproved = isApproved;
+    }
+
+    public void setApproved(boolean status) {
+        this.isApproved = status;
+    }
+
+    public void setSuggestionText(String updatedSuggestion) {
+        this.suggestionText = updatedSuggestion;
+    }
+
+    public CampInformation getCampInformation() { return this.campInformation; }
+    public String getStudentId() { return this.getStudentId(); }
+    public String getCampId() { return this.getCampId(); }
+}
