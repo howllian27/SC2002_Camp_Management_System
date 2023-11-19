8cde9b8989baeff3522bb88483ff1d287e7ace9a
Ruin9999 <b14ktoss@gmail.com>
2023-11-20 04:15:15 +0800

    Added utility classes

---
+++ Application/src/utils/Input.java
@@ -0,0 +1,40 @@
+package utils;
+
+import java.util.Scanner;
+
+/**
+ * Utility class for handling user input.
+ * This class provides methods for reading strings and integers from the console,
+ * with error handling for integer inputs.
+ *
+ * @author Shun Jie
+ * @version 1.0
+ */
+public class Input {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    /**
+     * Reads the next line of text from the console.
+     *
+     * @return A String representing the next line of text entered by the user.
+     */
+    public static String nextLine() {
+        return scanner.nextLine();
+    }
+
+    /**
+     * Reads the next integer from the console, displaying a warning message
+     * and prompting for input until a valid integer is entered.
+     *
+     * @return An integer entered by the user.
+     */
+    public static int nextInt() {
+        while(!scanner.hasNextInt()) {
+            Logger.Warning(Logger.LOGGER_WARNING_INTEGER);
+            scanner.nextLine();
+        }
+        int nextInt = scanner.nextInt();
+        scanner.nextLine();
+        return nextInt;
+    }
+}
---
+++ Application/src/utils/Logger.java
@@ -0,0 +1,50 @@
+package utils;
+
+/**
+ * Utility class for logging messages with color-coded output.
+ * Supports logging error, warning, and success messages in different colors.
+ *
+ * @author Shun Jie
+ * @version 1.0
+ */
+public class Logger {
+    public static final String LOGGER_WARNING_INTEGER = "Please enter a valid number.";
+
+    // ANSI escape codes for text colors
+    private static final String GREEN = "\u001B[32m";
+    private static final String YELLOW = "\u001B[33m";
+    private static final String RED = "\u001B[31m";
+    private static final String RESET = "\u001B[0m";
+
+    /**
+     * Logs an error message in red color.
+     *
+     * @param errorMessage The error message to be logged.
+     */
+    public static void Error(String errorMessage) {
+        print(RED + errorMessage);
+    }
+
+    /**
+     * Logs a warning message in yellow color.
+     *
+     * @param warningMessage The warning message to be logged.
+     */
+    public static void Warning(String warningMessage) {
+        print(YELLOW + warningMessage);
+    }
+
+    /**
+     * Logs a success message in green color.
+     *
+     * @param message The success message to be logged.
+     */
+    public static void Success(String message) {
+        print(GREEN + message);
+    }
+
+    //Private method to print the message with color and reset
+    private static void print(String message) {
+        System.out.println(message + RESET);
+    }
+}
